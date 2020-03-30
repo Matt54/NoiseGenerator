686cddde184859ebaf23f8dbc05e0905f8b7b064
Matt Pfeiffer <mattpfeiffer54@gmail.com>
2020-03-29 20:00:34 -0500

    Initial Commit

---
+++ NoiseGeneratorSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NoiseGeneratorSwiftUI
+//
+//  Created by Macbook on 3/29/20.
+//  Copyright © 2020 Matt Pfeiffer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
